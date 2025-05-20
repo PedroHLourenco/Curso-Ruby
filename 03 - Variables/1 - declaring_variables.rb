ac615c72298fca6d3a5c01a3868cec32d32d226b
Pedro Henrique <pedrohal2503@gmail.com>
2025-05-19 21:42:33 -0300

    Variables

---
+++ 03 - Variables/1 - declaring_variables.rb
@@ -0,0 +1,24 @@
+age = 21
+puts age
+
+name = "Pedro"
+last_name = "Lourenço"
+puts name
+puts last_name
+
+puts age + 7 # 21 + 7
+puts name + last_name
+
+age = 35
+puts age
+
+age_in_ten_years = age + 10 # 45
+puts age_in_ten_years
+
+age = age + 7 # 42
+puts age
+
+chameleon = 24
+chameleon = "Random text"
+chameleon = 3.14
+puts chameleon
